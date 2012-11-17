f1df97c6092167210e1c5d477d3c6ceb82a0d06e
Andrew Smillie <ajsmillie@gmail.com>
2012-11-17 21:50:39 +0000

    Refactoring to use seperate controllers

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -159,7 +159,24 @@ class UserController
     }
 
   }
-
+  
+  static private function generatePassword ($length = 8) {
+      $password = "";
+      $possible = "2346789bcdfghjkmnpqrtvwxyzBCDFGHJKLMNPQRTVWXYZ";
+      $maxlength = strlen($possible);
+      if ($length > $maxlength) {
+          $length = $maxlength;
+      }
+      $i = 0; 
+      while ($i < $length) { 
+          $char = substr($possible, mt_rand(0, $maxlength-1), 1);
+          if (!strstr($password, $char)) { 
+              $password .= $char;
+              $i++;
+          }
+      }
+      return $password;
+  }
   
   
 }
