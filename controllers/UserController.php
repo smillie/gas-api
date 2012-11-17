8ec2c665110b77f7d308fe48f432a96024c50fa0
Andrew Smillie <ajsmillie@gmail.com>
2012-11-17 22:02:07 +0000

    Refactoring code from generic functions class to user controller class

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -35,7 +35,7 @@ class UserController
 
     $output = array();
     foreach (array_slice($results, 1) as $ldap_user) {
-      $user = formatUserArray($ldap_user, $con);
+      $user = UserController::formatUserArray($ldap_user, $con);
       $output[] = $user;
     }
 
@@ -64,7 +64,7 @@ class UserController
       exit;
     }
 
-    $created = createLdapUser($con, $username, $firstname, $lastname, $studentnumber, $email);
+    $created = UserController::createLdapUser($con, $username, $firstname, $lastname, $studentnumber, $email);
     if ($created == true) {
       echo "{\"success\": \"$username created with password $created\"}";
     } else {
@@ -87,7 +87,7 @@ class UserController
     $result = ldap_get_entries($con, $search);
     exitIfNotFound($con, $search);
 
-    $output = formatUserArray($result[0], $con);
+    $output = UserController::formatUserArray($result[0], $con);
 
     echo json_encode($output);
   }
@@ -115,7 +115,7 @@ class UserController
         }
     } 
 
-    $groups = getGroupsForUser($con, $username);
+    $groups = UserController::getGroupsForUser($con, $username);
     foreach ($groups as $g) {
         $attr['memberUid'] = $username;
         ldap_mod_del($con, "cn=" . $g . ",ou=groups,dc=geeksoc,dc=org", $attr);
@@ -135,7 +135,7 @@ class UserController
     $search = ldap_search($con, $dn, "(uid=$username)");
     exitIfNotFound($con, $search);
 
-    $pass = generatePassword(); 
+    $pass = UserController::generatePassword(); 
     mt_srand((double)microtime()*1000000);
     $salt = pack("CCCCCCCC", mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand());
     $hashedpass = "{SSHA}" . base64_encode( sha1( $pass . $salt, true) . $salt );
@@ -177,6 +177,161 @@ class UserController
       }
       return $password;
   }
+  
+  static private function createLdapUser($con, $username, $firstname, $lastname, $studentnumber, $email) {
+    
+    global $dn;
+    
+    //compute uid
+    $users = ldap_get_entries($con, ldap_search($con, $dn, "(objectclass=posixaccount)"));
+    $uidno = 10000;
+    foreach($users as $u) {
+        if ($u['uidnumber'][0] > $uidno) 
+            $uidno = $u['uidnumber'][0];
+    }
+    $uidno += 1;
+    
+    //compute expiry date
+    $expiry = UserController::computeExpiry(date());
+    
+    //generate password
+    $pass = UserController::generatePassword(); 
+    mt_srand((double)microtime()*1000000);
+    $salt = pack("CCCCCCCC", mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand());
+    $hashedpass = "{SSHA}" . base64_encode( sha1( $pass . $salt, true) . $salt );
+    
+    $newuser['objectclass'][0] = "inetOrgPerson";
+    $newuser['objectclass'][1] = "organizationalPerson";
+    $newuser['objectclass'][2] = "person";
+    $newuser['objectclass'][3] = "top";
+    $newuser['objectclass'][4] = "posixAccount";
+    $newuser['objectclass'][5] = "shadowAccount";
+    $newuser['objectclass'][6] = "gsAccount";
+    $newuser['cn'] = "$firstname $lastname";
+    $newuser['sn'] = $lastname;
+    $newuser['givenName'] = $firstname;
+    $newuser['title'] = "Member";
+    $newuser['uid'] = $username;
+    $newuser['uidnumber'] = (int) $uidno;
+    $newuser['gidNumber'] = (int) 500;
+    $newuser['homeDirectory'] = "/home/$username";
+    $newuser['loginShell'] = "/bin/bash";
+    $newuser['gecos'] = "$firstname $lastname,,,";
+    $newuser['shadowLastChange'] = (int) 10877;
+    $newuser['shadowMax'] = (int) 99999;
+    $newuser['shadowWarning'] = (int) 7;
+    $newuser['mail'] = $email;
+    $newuser['studentNumber'] = (int) $studentnumber;
+    $newuser['hasPaid'] = "TRUE";
+    $newuser['hasSignedTOS'] = "TRUE";
+    $newuser['shadowExpire'] = (int) $expiry;
+    $newuser['userpassword'] = $hashedpass;
+    
+    //add to directory
+    ldap_add($con,"uid=$username,$dn", $newuser);
+    if (ldap_error($con) != "Success") {
+        $return = false;
+    }
+    //adduser to members group
+    $newmember['memberUid'] = $username;
+    ldap_mod_add($con, "cn=members,ou=groups,dc=geeksoc,dc=org", $newmember);
+    if (ldap_error($con) != "Success") {
+        $return = false;
+    }
+    
+    //irc notification
+    //email user details
+    //email gsag
+    
+    if (!isset($return)) {
+      $return = $pass;
+    }
+    return $return; 
+  }
+  
+  static private function getStatus($expiry, $paid="TRUE") {
+      $day = intval(time()/(60*60*24));
+      $i = (int)$expiry;
+      if (!isset($expiry)) $i=99999;
+
+      if ($i == 1) {
+          $status = "Administratively Disabled";
+      } elseif ($i <= $day) {
+          $status = "Expired";
+      } elseif ($i <= ($day+60) && $paid == "FALSE") {
+          $status = "Expiring (Not Paid)";
+      } elseif ($i <= ($day+60)) {
+          $status = "Expiring";
+      } elseif ($paid == "FALSE") {
+          $status = "Not Paid";
+      } else {
+          $status = "Active";
+      }
+
+      return "$status";
+  }
+  
+  static private function formatUserArray($ldap_user, $con) {
+    $user["username"] = $ldap_user["uid"][0];
+    $user["firstname"] = $ldap_user["givenname"][0];
+    $user["lastname"] = $ldap_user["sn"][0];
+    $user["displayname"] = $ldap_user["cn"][0];
+    $user["email"] = $ldap_user["mail"][0];
+    $user["title"] = $ldap_user["title"][0];
+    $user["studentnumber"] = $ldap_user["studentnumber"][0];
+    $user["status"] = UserController::getStatus($ldap_user["shadowexpire"][0]);
+    $user["expiry"] = toDate($ldap_user["shadowexpire"][0]);
+    $user["paid"] = toBoolean($ldap_user["haspaid"][0]);
+    $user["notes"] = $ldap_user["notes"][0]; //only populated if allowed to see...
+    $user["loginshell"] = $ldap_user["loginshell"][0];
+    $user["homedirectory"] = $ldap_user["homedirectory"][0];
+    $user["uidnumber"] = $ldap_user["uidnumber"][0];
+    $user["gidnumber"] = $ldap_user["gidnumber"][0];
+    $user["groups"] = UserController::getGroupsForUser($con, $user["username"]);
+    
+    $sshkeys = array();
+    foreach (array_slice($ldap_user["sshpublickey"], 1) as $key) {
+      $sshkeys[] = $key;
+    }
+    $user["sshkeys"] = $sshkeys;
+    
+    return $user;
+  }
+  
+  static private function computeExpiry($date) { //not correct...
+	
+    $nextExpiry = strtotime('first Friday of October', $date);
+    $threshold = strtotime('last Friday of May', $date);
+      
+	  if ($date < $threshold) {
+		  $expiry = $nextExpiry;
+	  } else {
+		 $expiry = strtotime('first Friday of October', $date + 365 * 24 * 60 * 60);
+	 }
+   $expiry = $expiry/(24 * 60 * 60);
+	 return $expiry;        
+  }
+  
+  static private function getGroupsForUser($con, $user) {
+      $groups = array();
+      foreach (getAllGroups($con) as $groupEntry) {
+          if (UserController::isUserInGroup($con, $user, $groupEntry['cn'][0])) {
+              $groups[] = $groupEntry['cn'][0];
+          }
+      }
+      return $groups;
+  }
+  
+  static private function isUserInGroup($con, $user, $group) {
+      $group_search = ldap_search($con, "cn=$group,ou=groups,dc=geeksoc,dc=org", "(memberUid=$user)");
+      if (ldap_count_entries($con, $group_search) >= 1) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+
+  
   
   
 }
--- includes/functions.php
+++ includes/functions.php
@@ -1,25 +1,5 @@
 <?php
-  function getStatus($expiry, $paid="TRUE") {
-      $day = intval(time()/(60*60*24));
-      $i = (int)$expiry;
-      if (!isset($expiry)) $i=99999;
 
-      if ($i == 1) {
-          $status = "Administratively Disabled";
-      } elseif ($i <= $day) {
-          $status = "Expired";
-      } elseif ($i <= ($day+60) && $paid == "FALSE") {
-          $status = "Expiring (Not Paid)";
-      } elseif ($i <= ($day+60)) {
-          $status = "Expiring";
-      } elseif ($paid == "FALSE") {
-          $status = "Not Paid";
-      } else {
-          $status = "Active";
-      }
-
-      return "$status";
-  }
   
   function toDate($datestring) {
     $expiry =  intval($datestring)*(60*60*24);
@@ -34,25 +14,6 @@
     }
   }
   
-  function getGroupsForUser($con, $user) {
-      $groups = array();
-      foreach (getAllGroups($con) as $groupEntry) {
-          if (isUserInGroup($con, $user, $groupEntry['cn'][0])) {
-              $groups[] = $groupEntry['cn'][0];
-          }
-      }
-      return $groups;
-  }
-  
-  function isUserInGroup($con, $user, $group) {
-      $group_search = ldap_search($con, "cn=$group,ou=groups,dc=geeksoc,dc=org", "(memberUid=$user)");
-      if (ldap_count_entries($con, $group_search) >= 1) {
-        return true;
-      } else {
-        return false;
-      }
-  }
-
   function getAllGroups($con) {
       $group_search = ldap_search($con, "ou=groups,dc=geeksoc,dc=org", "(objectClass=posixGroup)");
       ldap_sort($con, $group_search, 'cn');
@@ -61,32 +22,6 @@
       return array_slice($results, 1);
   }
   
-  function formatUserArray($ldap_user, $con) {
-    $user["username"] = $ldap_user["uid"][0];
-    $user["firstname"] = $ldap_user["givenname"][0];
-    $user["lastname"] = $ldap_user["sn"][0];
-    $user["displayname"] = $ldap_user["cn"][0];
-    $user["email"] = $ldap_user["mail"][0];
-    $user["title"] = $ldap_user["title"][0];
-    $user["studentnumber"] = $ldap_user["studentnumber"][0];
-    $user["status"] = getStatus($ldap_user["shadowexpire"][0]);
-    $user["expiry"] = toDate($ldap_user["shadowexpire"][0]);
-    $user["paid"] = toBoolean($ldap_user["haspaid"][0]);
-    $user["notes"] = $ldap_user["notes"][0]; //only populated if allowed to see...
-    $user["loginshell"] = $ldap_user["loginshell"][0];
-    $user["homedirectory"] = $ldap_user["homedirectory"][0];
-    $user["uidnumber"] = $ldap_user["uidnumber"][0];
-    $user["gidnumber"] = $ldap_user["gidnumber"][0];
-    $user["groups"] = getGroupsForUser($con, $user["username"]);
-    
-    $sshkeys = array();
-    foreach (array_slice($ldap_user["sshpublickey"], 1) as $key) {
-      $sshkeys[] = $key;
-    }
-    $user["sshkeys"] = $sshkeys;
-    
-    return $user;
-  }
   
   function formatGroupArray($ldap_group, $con) {
     $group["name"] = $ldap_group["cn"][0];
@@ -137,89 +72,6 @@
   }
   
   
-  function computeExpiry($date) { //not correct...
-	
-    $nextExpiry = strtotime('first Friday of October', $date);
-    $threshold = strtotime('last Friday of May', $date);
-      
-	  if ($date < $threshold) {
-		  $expiry = $nextExpiry;
-	  } else {
-		 $expiry = strtotime('first Friday of October', $date + 365 * 24 * 60 * 60);
-	 }
-   $expiry = $expiry/(24 * 60 * 60);
-	 return $expiry;        
-  }
   
-  function createLdapUser($con, $username, $firstname, $lastname, $studentnumber, $email) {
-    
-    global $dn;
-    
-    //compute uid
-    $users = ldap_get_entries($con, ldap_search($con, $dn, "(objectclass=posixaccount)"));
-    $uidno = 10000;
-    foreach($users as $u) {
-        if ($u['uidnumber'][0] > $uidno) 
-            $uidno = $u['uidnumber'][0];
-    }
-    $uidno += 1;
-    
-    //compute expiry date
-    $expiry = computeExpiry(date());
-    
-    //generate password
-    $pass = generatePassword(); 
-    mt_srand((double)microtime()*1000000);
-    $salt = pack("CCCCCCCC", mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand());
-    $hashedpass = "{SSHA}" . base64_encode( sha1( $pass . $salt, true) . $salt );
-    
-    $newuser['objectclass'][0] = "inetOrgPerson";
-    $newuser['objectclass'][1] = "organizationalPerson";
-    $newuser['objectclass'][2] = "person";
-    $newuser['objectclass'][3] = "top";
-    $newuser['objectclass'][4] = "posixAccount";
-    $newuser['objectclass'][5] = "shadowAccount";
-    $newuser['objectclass'][6] = "gsAccount";
-    $newuser['cn'] = "$firstname $lastname";
-    $newuser['sn'] = $lastname;
-    $newuser['givenName'] = $firstname;
-    $newuser['title'] = "Member";
-    $newuser['uid'] = $username;
-    $newuser['uidnumber'] = (int) $uidno;
-    $newuser['gidNumber'] = (int) 500;
-    $newuser['homeDirectory'] = "/home/$username";
-    $newuser['loginShell'] = "/bin/bash";
-    $newuser['gecos'] = "$firstname $lastname,,,";
-    $newuser['shadowLastChange'] = (int) 10877;
-    $newuser['shadowMax'] = (int) 99999;
-    $newuser['shadowWarning'] = (int) 7;
-    $newuser['mail'] = $email;
-    $newuser['studentNumber'] = (int) $studentnumber;
-    $newuser['hasPaid'] = "TRUE";
-    $newuser['hasSignedTOS'] = "TRUE";
-    $newuser['shadowExpire'] = (int) $expiry;
-    $newuser['userpassword'] = $hashedpass;
-    
-    //add to directory
-    ldap_add($con,"uid=$username,$dn", $newuser);
-    if (ldap_error($con) != "Success") {
-        $return = false;
-    }
-    //adduser to members group
-    $newmember['memberUid'] = $username;
-    ldap_mod_add($con, "cn=members,ou=groups,dc=geeksoc,dc=org", $newmember);
-    if (ldap_error($con) != "Success") {
-        $return = false;
-    }
-    
-    //irc notification
-    //email user details
-    //email gsag
-    
-    if (!isset($return)) {
-      $return = $pass;
-    }
-    return $return; 
-  }
   
 ?>
