d2b55a6473a725b67381e6f048a3066eb864863f
Andrew Smillie <ajsmillie@gmail.com>
2013-04-03 20:26:23 +0100

    Adding DB schema file

---
+++ schema/db.sql
@@ -0,0 +1,47 @@
+--
+-- Database for  gas-api
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newusers`
+--
+
+CREATE TABLE `newusers` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(35) NOT NULL,
+  `lastname` varchar(35) NOT NULL,
+  `username` varchar(36) NOT NULL,
+  `studentnumber` int(9) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `IS_DELETED` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nominations`
+--
+
+CREATE TABLE `nominations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nominee` varchar(255) NOT NULL,
+  `nominator` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `positions`
+--
+
+CREATE TABLE `positions` (
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
